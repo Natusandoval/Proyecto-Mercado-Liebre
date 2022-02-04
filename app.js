f2bff74927ff9b5b43481d5d70b96a7da6bc74f7
natusandoval <sandoval_natalia@ymail.com>
2022-02-03 22:35:38 -0300

    cambios heroku

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express ();
 const publicPath = path.resolve (__dirname, "./public");
 app.use(express.static (publicPath));
 
-app.listen (3000, () => {console.log ("Servicor Corriendo Correctamente")});
+app.listen (process.env.PORT || 3000, function () {console.log ("Servicor corriendo en el puerto 3000")});
 
 app.get ("/", function (req, res) {res.sendFile (path.resolve (__dirname, "./views/home.html"))})
 
