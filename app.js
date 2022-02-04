dea4f77b0c083bdbb0c08658d7f76ba759fcf42a
natusandoval <sandoval_natalia@ymail.com>
2022-02-03 22:05:02 -0300

    proyecto mercadoliebre

--- app.js
+++ app.js
@@ -1,31 +1,14 @@
-let autos = [ 
-    {
-marca : "ford", 
-modelo : "fiesta",
-let autos = [ 
-    {
-marca : "ford", 
-modelo : "fiesta",
-precio : 150000,
-km : 200,
-color : "azul",
-cuotas : 12,
-anio : 2019,
-patente : "APL123",
-vendido : false
-    },
-    {
-marca : "toyota", 
-modelo : "corolla",
-precio : 100000,
-km : 0,
-color : "blanco",
-cuotas : 14,
-anio : 2019,
-patente : "JJK116",
-vendido : false
-}
-]
+const express = require ("express");
+const path = require ("path");
+const app = express ();
 
-module.exports = autos
-console.log (autos)
+const publicPath = path.resolve (__dirname, "./public");
+app.use(express.static (publicPath));
+
+app.listen (3000, () => {console.log ("Servicor Corriendo Correctamente")});
+
+app.get ("/", function (req, res) {res.sendFile (path.resolve (__dirname, "./views/home.html"))})
+
+app.get ("/register", function (req, res) {res.sendFile (path.resolve (__dirname, "./views/register.html"))})
+
+app.get ("/login", function (req, res) {res.sendFile (path.resolve (__dirname, "./views/login.html"))})
